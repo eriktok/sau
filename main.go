24e291d20cd618ec8d0a11840d32a62ae9ca85a4
“erto” <erikbt13@uia.no>
2022-12-10 15:18:08 +0100

    works but need some more refactoring.

--- main.go
+++ main.go
@@ -2,29 +2,18 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"net/url"
 	"os"
 )
 
-/*
-Type strcut
--host
-- url path
--file path
-*/
-
-type Data struct {
-	host string
-	path string
-}
-type Datas struct {
-	d []Data
-}
-
 func main() {
 	urls := make([]string, 0)
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		urls = append(urls, scanner.Text())
@@ -35,10 +24,31 @@ func main() {
 	}
 
 	pu := parseUrls(urls)
-	for _, v := range pu {
-		fmt.Println(v)
+	for k, v := range pu {
+		createDirectoryIfNotExists(currentDir + "/" + k)
+		createFile(currentDir+"/"+k+"/"+k, v)
+	}
+}
+
+func createDirectoryIfNotExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, os.ModeDir|0755)
+	}
+	return nil
+}
+
+func createFile(path string, text []string) {
+	file, err := os.Create(path + ".txt")
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
 	}
 
+	datawriter := bufio.NewWriter(file)
+	for _, data := range text {
+		_, _ = datawriter.WriteString(data + "\n")
+	}
+	datawriter.Flush()
+	file.Close()
 }
 
 func parseUrls(urls []string) map[string][]string {
